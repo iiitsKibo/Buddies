e47883915e71ef78d608f177067caf09bdf90933
raqeeba <Raqeeb.Adams@redpandasoftware.co.za>
2022-07-22 14:40:19 +0200

    Add project files.

---
+++ Buddies/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Buddies
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            CallWebAPIAsync()
+                .Wait();
+        }
+        static async Task CallWebAPIAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://swapi.dev/api/people");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync(client.BaseAddress);
+
+                var buddies = new Dictionary<string, List<string>>(); 
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = await response.Content.ReadAsAsync<StarWarsData>();
+
+                    foreach (var item in data.results)
+                    {
+                        var buddieKey = "";
+                        foreach (var film in item.films)
+                        {
+                            var flimNum = film.Substring(film.Length - 2).Substring(0,1);
+
+                            buddieKey += flimNum;
+                        }
+                        if (buddies.ContainsKey(buddieKey))
+                        {
+                            buddies[buddieKey].Add(item.name);
+                        }
+                        else
+                        {
+                            buddies.Add(buddieKey,new List<string>() { item.name });
+                        }
+                       
+
+                    }
+
+                    foreach (var key in buddies.Keys)
+                    {
+                       
+                        foreach (var characterName in buddies[key])
+                        {
+                            Console.Write($"{characterName}, ");
+                        }
+                        Console.WriteLine();
+                    }
+                   
+                    
+                }
+            }
+        }
+    }
+}
---
+++ Buddies/StarWarsData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buddies
+{
+    public class StarWarsData
+    {
+
+        public int count { get; set; }
+        public string next { get; set; }
+        public object previous { get; set; }
+        public Result[] results { get; set; }
+
+    }
+
+
+}
